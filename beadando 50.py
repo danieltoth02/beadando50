7ba3541ac012c8ff3ce61739bf94897f6a4ae4b0
Tóth Dániel <63225508+danieltoth02@users.noreply.github.com>
2020-04-24 12:32:00 +0200

    Add files via upload

---
+++ beadando 50.py
@@ -0,0 +1,18 @@
+n=int(input("Utszakasz hossza (km)= "))
+m=int(input("Megengedett sebesseg (km/h)= "))
+rendszam_lista=[]
+sebesseg_lista=[]
+while True:
+    ir=input("Ido, rendszamtabla: ")
+    if ir=="end":
+        break
+    ir2=ir.split(" ")
+    ido1 = int(ir2[0]) * 3600 + int(ir2[1]) * 60 + int(ir2[2])
+    ido2 = int(ir2[3]) * 3600 + int(ir2[4]) * 60 + int(ir2[5])
+    idohossz=(ido2-ido1)/3600
+    sebesseg=n/idohossz
+    if sebesseg>m:
+        sebesseg_lista.append(sebesseg)
+        rendszam_lista.append(ir2[6])
+for i in range (len(rendszam_lista)):
+    print (rendszam_lista[i],str(f'{sebesseg_lista[i]:.2f}')+"km/h")
